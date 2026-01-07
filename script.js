cb2a3d1a7b6433ae3379dd52b1fa75911e055516
adyantwarang <waranganmol@gmail.com>
2026-01-07 21:04:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function myFunction() {
-    var a = 4;
-    document.getElementById("demo").innerHTML = a*a;
-}
+function validateEmail(e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgBox = document.getElementById('message');
+
+    let message = '';
+
+    if (email === '') {
+        message += 'Email is required. ';
+         
+    } else if (age === '') {
+        message += 'Age is required. ';
+    } else }
+        message +  'country is required. ';
+
+    if (pass === '') {
+        <button>
+            Submit
+        </button>
